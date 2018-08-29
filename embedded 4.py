ef206727701cff8461567de0be86cbab449a4ee3
Yohennas <yohdude@gmail.com>
2018-08-29 23:11:21 +0000

    first template

---
+++ embedded 4.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import Adafruit_MCP3008
+import time
+from os import system, name
+GPIO.setmode(GPIO.BCM)
+StartTimer = time.time()
+
+# pin definition
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+switch_1 = 
+switch_2 =
+switch_3 = 
+
+# pin setup
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+GPIO.setup(switch_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI,
+miso=SPIMISO)
+
+def FreqChng(channel):    
+    if (freq==2):
+        freq=0.5
+    else:
+        freq*=2
+
+def Reset(channel):
+    EndTimer = 0
+    _ = system('clear')
+    
+GPIO.add_event_detect(switch_1, GPIO.FALLING, callback=FreqChng,
+bouncetime=200)    
+    
+GPIO.add_event_detect(switch_2, GPIO.FALLING, callback=Reset,
+bouncetime=200)
+
+
+# global variable
+values = [0]*8
+
+freq = 0.5
+
+while True:    
+    for i in range(8):
+        
+        values[i] = mcp.read_adc(i)
+        # delay for a half second
+        time.sleep(freq)
+        
+    EndTimer = time.time()
+    Timer = EndTimer-StartTimer
+    Time = time.time()
+    print(Time)
+    print (Timer)
+    print (values)   
+    
+        
+        
+
+
