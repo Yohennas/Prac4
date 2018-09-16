e1859b66dfae5c356843d1d3fa9635835680e8eb
t3mp4st <gild.adam8@gmail.com>
2018-09-16 11:50:37 +0000

    embedded 4.py

--- embedded 4.py
+++ embedded 4.py
@@ -5,7 +5,7 @@ import time
 from os import system, name
 GPIO.setmode(GPIO.BCM)
 StartTimer = time.time()
-boolean stop=false
+stop=False
 
 # pin definition
 SPICLK = 11
@@ -13,9 +13,9 @@ SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
 switch_1 = 16
-switch_2 = 20
-switch_3 = 21
-switch_4 = 19
+switch_2 = 6
+switch_3 = 26
+switch_4 = 5
 
 # pin setup
 GPIO.setup(SPIMOSI, GPIO.OUT)
@@ -24,27 +24,30 @@ GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 GPIO.setup(switch_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(switch_4, GPIO.IN, pull_up_down=GPIO.PUD.UP)
+GPIO.setup(switch_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI,
 miso=SPIMISO)
 
-def FreqChng(channel):    
+def FreqChng(channel):
+    global freq    
     if (freq==2):
         freq=0.5
     else:
         freq*=2
 
 def Reset(channel):
-    EndTimer = 0
+    global StartTimer
+    StartTimer=time.time()
     _ = system('clear')
 
 def Stop(channel):
-	if stop=false:
-		stop=true
-	else if stop=true:
-		stop=false
+	global stop
+	if (stop==False):
+		stop=True
+	else:
+		stop=False
 	    
 GPIO.add_event_detect(switch_1, GPIO.FALLING, callback=FreqChng,
 bouncetime=200)    
@@ -56,18 +59,21 @@ GPIO.add_event_detect(switch_3, GPIO.RISING, callback=Stop, bouncetime=200)
 
 # global variable
 values = [0]*8
-
 freq = 0.5
 
-while (stop=false):    
-    for i in range(8):
+while (1):
+    if (stop==False):    
+        for i in range(8):
         
-        values[i] = mcp.read_adc(i)
-	
+            values[i] = mcp.read_adc(i)
+	    print(values[i])
     EndTimer = time.time()
     Timer = EndTimer-StartTimer
     Time = time.time()
-    print(Time ' ' Timer ' ' values)
+    Time = str(Time)
+    Timer = str(Timer)
+    if (stop ==False):
+        print(Time+' '+Timer)
     time.sleep(freq)	   
     
         
