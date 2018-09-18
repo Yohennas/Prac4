0527e24a0ef77a368d28c586e40a06759670a052
Yohennas <36073916+Yohennas@users.noreply.github.com>
2018-09-18 12:36:09 +0200

    Update embedded 4.py

--- embedded 4.py
+++ embedded 4.py
@@ -5,6 +5,8 @@ import time
 from os import system, name
 GPIO.setmode(GPIO.BCM)
 StartTimer = time.time()
+stop=False
+
 
 # pin definition
 SPICLK = 11
@@ -12,9 +14,9 @@ SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
 switch_1 = 
-switch_2 =
-switch_3 =
-switch_4 =
+switch_2 = 
+switch_3 = 
+switch_4 = 
 
 # pin setup
 GPIO.setup(SPIMOSI, GPIO.OUT)
@@ -23,70 +25,85 @@ GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 GPIO.setup(switch_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI,
 miso=SPIMISO)
 
-def FreqChng(channel):    
+def FreqChng(channel):
+    global freq    
     if (freq==2):
         freq=0.5
     else:
         freq*=2
 
 def Reset(channel):
-    EndTimer = 0
+    global StartTimer
+    StartTimer=time.time()
     _ = system('clear')
-    
+
+def Stop(channel):
+	global stop
+	if (stop==False):
+		stop=True
+	else:
+		stop=False
+
+def Display(channel):
 
     
     
-
+    
+    
+    
+    
+    
+    
+    
+                	
+                	
+                	
+                	
+                	
+                	
+                	
+						
+        	       
+	
+	    
 GPIO.add_event_detect(switch_1, GPIO.FALLING, callback=FreqChng,
 bouncetime=200)    
     
 GPIO.add_event_detect(switch_2, GPIO.FALLING, callback=Reset,
 bouncetime=200)
 
+GPIO.add_event_detect(switch_3, GPIO.RISING, callback=Stop, bouncetime=200)
 
-GPIO.add_event_detect(switch_4, GPIO.FALLING, callback=Display,
-bouncetime=200)   
-
+GPIO.add_event_detect(switch_4, GPIO.FALLING, callback=Display, bouncetime=200)
 
 # global variable
 values = [0]*8
-past_values = [[0]*8]*5
-j=0
-
 freq = 0.5
-print(' {0:>4}  {1:>4}  {2:>4}  {3:>4}  {4:>4}  {5:>4}  {6:>4}  {7:>4} '.format(*range(8))) #headings
-print('-' * 57)
+j=0
+past_values = [[0]*8]*5
 
-while True:
-    
-    if (j==5):
-        j=0
+
+print('_______________________________________')
+print('Time      Timer     Pot    Temp   Light')
+while (1):
         
     for i in range(8):
         
         values[i] = mcp.read_adc(i)
-        past_values[j][i]
-        
-        # delay for a half second
-        time.sleep(freq)
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values)) #values
-        
-    EndTimer = time.time()
+	if (j<5):
+		past_values[j][i]=mcp.read_adc(i)
+	   
+    EndTimer=time.time()
     Timer = EndTimer-StartTimer
-    Time = time.time()
-    print(Time)
-    print (Timer)
-    print (values)   
-    j+=1
+    Time = time.asctime( time.localtime(time.time()))
+    Time = Time[11 : 19]
+    Timer = time.asctime(time.localtime(Timer))
+    Timer = Timer[11 : 19]
     
-    
-    
-        
-            
-
-
+    if (stop==False):
